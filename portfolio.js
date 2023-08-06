60cf1354892ae4c97df7a4348c6c731168776986
Mihir M. Parmar <110229768+mihirparmarofficial@users.noreply.github.com>
2023-08-06 10:43:49 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,37 @@
+/* toogle icon navbar */ 
+let mennubar = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/* scroll section active link */ 
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id + ']').classList.add('active');
+            });
+        };
+    });
+    /* sticky navbar */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+     /* remove toggle icon and navbar when click navbar link (scroll) */
+     menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
